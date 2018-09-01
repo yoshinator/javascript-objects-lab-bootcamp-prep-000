1ef6c169db24a6835896ffd9d0ccee882e3410ab
yoan ante <yoanante@gmail.com>
2018-09-01 22:20:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = new Object({});
+var recipes = new Object({"scrambled eggs": "add two eggs, cheddar cheese and ham", "key2": "The second value for key 2"});
 
 function updateObjectWithKeyAndValue(object, key, value){
   var newObject = object;
