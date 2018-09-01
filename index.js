bb75a99468e47831a55bf68e0a2f75b4225b3cd9
yoan ante <yoanante@gmail.com>
2018-09-01 22:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = new Object({});
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({},object,{key, value});
+  Object.assign(object,{key, value});
   return object;
 }
