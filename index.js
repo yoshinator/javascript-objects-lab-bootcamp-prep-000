1db2b7fd1aca5e9e88c4df385eeaf4b550a74e0b
yoan ante <yoanante@gmail.com>
2018-09-01 22:19:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var recipes = new Object({});
 
 function updateObjectWithKeyAndValue(object, key, value){
   var newObject = object;
+  console.log(object);
+  console.log(newObject);
   newObject[value] = key;
+  console.log(object);
+  console.log(newObject);
   return object;
 }
+
+updateObjectWithKeyAndValue(recipes, 'sandwiches', 'add virgina honey ham, swiss cheese and mayonaise put on two slices of bread and cut in half.')
