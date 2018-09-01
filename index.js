bc8da0c8233b25840728fac43e83f1cd2da312a6
yoan ante <yoanante@gmail.com>
2018-09-01 22:34:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectKeyAndValue(object, key, value){
-  object[key]=value;
+  object[key] = value;
   return object;
 }
